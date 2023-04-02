1380e5bb53f6e51cd842d17037e6a94e188e4b64
Tal Barda <talbarda18@gmail.com>
2023-04-02 23:12:32 +0300

    Manager Main - Part-4 redesign the manager main page with recyclerView Finish the Workers List Mockup Started ShiftRequest Fragment (need to continue Tomorrow)

--- app/src/main/java/com/example/myapplication/ManagerFragment.java
+++ app/src/main/java/com/example/myapplication/ManagerFragment.java
@@ -4,17 +4,92 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.Model.ManagerButton;
 import com.example.myapplication.databinding.FragmentManagerMainBinding;
+
+import java.util.ArrayList;
 
 public class ManagerFragment extends Fragment {
 
     private FragmentManagerMainBinding binding;
 
+    ArrayList<ManagerButton> buttonsArrayList = new ArrayList<ManagerButton>() {{
+        add(new ManagerButton("Workers List",R.id.WorkersListFragment));
+        add(new ManagerButton("Shift Requests",R.id.ShiftsRequestsFragment));
+        add(new ManagerButton("Workers Constrains",R.id.WorkersConstrainsFragment));
+        add(new ManagerButton("Shift Scheduling Status",R.id.ScheduleStatusFragment));
+        add(new ManagerButton("Schedule Worker Into Shift",R.id.ScheduleWorkerIntoShiftsFragment));
+        add(new ManagerButton("Define Shift Requirements",R.id.DefineShiftRequirementsFragment));
+    }};
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.date);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    public ManagerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentManagerMainBinding.inflate(inflater, container, false);
+        binding.rvManagerMain.setAdapter(new RecyclerView.Adapter<ViewHolder>() {
+
+            @NonNull
+            @Override
+            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                                     int viewType) {
+                View view =
+                        LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_one_line,
+                                parent, false);
+                return new ViewHolder(view);
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+                (holder).getTextView().setText(buttonsArrayList.get(position).getButtonName());
+                holder.getTextView().setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        NavHostFragment.findNavController(ManagerFragment.this)
+                                .navigate(buttonsArrayList.get(position).getFragment());
+                    }
+                });
+            }
+
+            @Override
+            public int getItemCount() {
+                return buttonsArrayList.size();
+            }
+        });
+        binding.rvManagerMain.setLayoutManager(new LinearLayoutManager(requireContext()));
+        return binding.getRoot();
+
+    }
+    /*
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -75,7 +150,7 @@ public class ManagerFragment extends Fragment {
             }
         });
     }
-
+*/
     @Override
     public void onDestroyView() {
         super.onDestroyView();
--- app/src/main/java/com/example/myapplication/WorkersListFragment.java
+++ app/src/main/java/com/example/myapplication/WorkersListFragment.java
@@ -1,26 +1,111 @@
 package com.example.myapplication;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.Model.Profile;
 import com.example.myapplication.databinding.FragmentWorkersListBinding;
+
+import java.util.ArrayList;
 
 public class WorkersListFragment extends Fragment {
     private FragmentWorkersListBinding binding;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.date);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    ArrayList<Profile> profileArrayList= new ArrayList<Profile>() {{
+        add(new Profile("tal","tal","tal@gmail.com","050-1234567"));
+        add(new Profile("Gal","Gal","Gal@gmail.com","050-1234567"));
+        add(new Profile("Yuval","Yuval","Yuval@gmail.com","050-1234567"));
+        add(new Profile("tal","tal","tal@gmail.com","050-1234567"));
+        add(new Profile("Gal","Gal","Gal@gmail.com","050-1234567"));
+        add(new Profile("Yuval","Yuval","Yuval@gmail.com","050-1234567"));
+        add(new Profile("tal","tal","tal@gmail.com","050-1234567"));
+        add(new Profile("Gal","Gal","Gal@gmail.com","050-1234567"));
+        add(new Profile("Yuval","Yuval","Yuval@gmail.com","050-1234567"));
+        add(new Profile("tal","tal","tal@gmail.com","050-1234567"));
+        add(new Profile("Gal","Gal","Gal@gmail.com","050-1234567"));
+        add(new Profile("Yuval","Yuval","Yuval@gmail.com","050-1234567"));
+        add(new Profile("tal","tal","tal@gmail.com","050-1234567"));
+        add(new Profile("Gal","Gal","Gal@gmail.com","050-1234567"));
+        add(new Profile("Yuval","Yuval","Yuval@gmail.com","050-1234567"));
+    }};
+
+    public WorkersListFragment() {
+        // Required empty public constructor
+    }
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+
         binding = FragmentWorkersListBinding.inflate(inflater, container, false);
+        binding.rvWorkersList.setAdapter(new RecyclerView.Adapter<ViewHolder>() {
+
+            @NonNull
+            @Override
+            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                               int viewType) {
+                View view =
+                        LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_one_line,
+                                parent, false);
+                return new ViewHolder(view);
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+                (holder).getTextView().setText(profileArrayList.get(position).getFirstName()+"\t\t\t"+
+                        profileArrayList.get(position).getLastName()+"\t\t\t"+
+                        profileArrayList.get(position).getPhoneNumber());
+                holder.getTextView().setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new AlertDialog.Builder(requireContext())
+                                .setTitle(((TextView) v).getText().toString())
+                                .setMessage(profileArrayList.get(position).getEmail())
+                                .setPositiveButton("Yay", (dialog, which) -> {
+
+                                }).setNegativeButton("Nay",(d,w)->{})
+                                .setOnDismissListener(dialog -> {
+
+                                })
+                                .create().show();
+                    }
+                });
+            }
+
+            @Override
+            public int getItemCount() {
+                return profileArrayList.size();
+            }
+        });
+        binding.rvWorkersList.setLayoutManager(new LinearLayoutManager(requireContext()));
         return binding.getRoot();
+
     }
 
     @Override
