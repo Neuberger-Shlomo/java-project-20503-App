ce7c34af9a4236bcde394bfcaafab8e7789694b6
Shlomo Neuberger <Shlomo@xoltar.com>
2023-04-15 03:26:20 +0300

    interface - update names - add custom OnClickListener

--- app/src/main/java/com/example/myapplication/Common/Views/ViewHolder/OneLiner/interfaces.java
+++ app/src/main/java/com/example/myapplication/Common/Views/ViewHolder/OneLiner/interfaces.java
@@ -1,14 +1,20 @@
 package com.example.myapplication.Common.Views.ViewHolder.OneLiner;
 
+import android.view.View;
+
 import androidx.annotation.NonNull;
 
 public final class interfaces {
-    public interface filterMethod<ObjectType,Query>{
+    public interface FilterMethod<ObjectType,Query>{
         public boolean shouldFilter(ObjectType item,Query query);
     }
-    public interface onBindViewHolderListener<Item,ViewHolder>
+    public interface OnBindViewHolderListener<Item,ViewHolder>
     {
         public void onBindViewHolder(@NonNull Item item,@NonNull ViewHolder holder, int position);
+    }
+
+    public interface OnItemClickListener<T>{
+        public void onClick(T item, View v);
     }
 
 }
