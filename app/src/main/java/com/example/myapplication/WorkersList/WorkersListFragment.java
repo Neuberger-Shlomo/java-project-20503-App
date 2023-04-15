9c71e4bd4d372898d9416564d2fa072048175372
Shlomo Neuberger <Shlomo@xoltar.com>
2023-04-16 00:02:52 +0300

    Delete duplicated views

--- app/src/main/java/com/example/myapplication/WorkersList/WorkersListFragment.java
+++ app/src/main/java/com/example/myapplication/WorkersList/WorkersListFragment.java
@@ -2,27 +2,17 @@ package com.example.myapplication.WorkersList;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelStoreOwner;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.Common.Views.Fragments.DateListFragment;
 import com.example.myapplication.Model.Profile;
-import com.example.myapplication.R;
 import com.example.myapplication.ViewModel.UserViewModel;
 import com.example.myapplication.ViewModel.WorkersViewModel;
 import com.example.myapplication.api.Api;
-import com.example.myapplication.databinding.FragmentWorkersListBinding;
 
 import java.util.ArrayList;
 
