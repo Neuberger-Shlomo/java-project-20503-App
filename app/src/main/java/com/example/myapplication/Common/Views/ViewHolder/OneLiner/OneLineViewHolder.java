527063e3d64b5fded5f6d1baf3178b81dda09579
Yossi Shemer <26yossi262@gmail.com>
2023-04-29 07:07:00 +0300

    added comments

--- app/src/main/java/com/example/myapplication/Common/Views/ViewHolder/OneLiner/OneLineViewHolder.java
+++ app/src/main/java/com/example/myapplication/Common/Views/ViewHolder/OneLiner/OneLineViewHolder.java
@@ -9,37 +9,60 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
 
+/**
+ * view holder for a one line item.
+ * @param <Item> the type of the item in the view holder
+ */
 public class OneLineViewHolder<Item> extends RecyclerView.ViewHolder {
     private final TextView textView;
+
     private final CardView cardView;
 
     private final View superView;
 
     private Item item;
 
+    /**
+     * Creates a new OneLineViewHolder instance with the given view.
+     *
+     * @param view the view to display in the view holder
+     */
     public OneLineViewHolder(View view) {
         super(view);
         superView = view;
         textView = (TextView) view.findViewById(R.id.line1);
         cardView = (CardView) view.findViewById(R.id.cardView0);
-
     }
 
+    /**
+     * @return the item in the view holder
+     */
     public Item getItem() {
         return item;
     }
 
+    /**
+     * save the current item in the view holder.
+     * @param item the item to save
+     */
     public void setItem(Item item) {
         this.item = item;
     }
 
-    public void setText(String text){
+    /**
+     * set the text for TextView in holder.
+     *
+     * @param text
+     */
+    public void setText(String text) {
         textView.setText(text);
     }
 
-    public void setOnClickListener(interfaces.OnItemClickListener<Item> l){
+    /**
+     * Sets an item click listener for the view holder.
+     * @param l the listener
+     */
+    public void setOnClickListener(interfaces.OnItemClickListener<Item> l) {
         superView.setOnClickListener(v -> l.onClick(item, v));
     }
-
-
 }
--- app/src/main/java/com/example/myapplication/Common/Views/ViewHolder/OneLiner/interfaces.java
+++ app/src/main/java/com/example/myapplication/Common/Views/ViewHolder/OneLiner/interfaces.java
@@ -4,17 +4,56 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 
+/**
+ * interface that is used in one liner view holder. and one liner adapter.
+ * for operations filtering, binding, and click events.
+ */
 public final class interfaces {
 
-    public interface FilterMethod<ObjectType,Query>{
-        public boolean shouldFilter(ObjectType item,Query query);
+    /**
+     * Interface for defining a filtering method.
+     *
+     * @param <ObjectType>  the object to filter
+     * @param <Query>       the query for filtering.
+     */
+    public interface FilterMethod<ObjectType, Query> {
+        /**
+         * return if want to filter item or not
+         *
+         * @param item  the object we checke
+         * @param query
+         * @return True if we want to filter
+         */
+        public boolean shouldFilter(ObjectType item, Query query);
     }
 
-    public interface OnBindViewHolderListener<Item,ViewHolder>{
-        public void onBindViewHolder(@NonNull Item item,@NonNull ViewHolder holder, int position);
+    /**
+     * define the binding action of ViewHolder.
+     * @param <Item>      the type of the item we want to bind
+     * @param <ViewHolder> the type of the ViewHolder
+     */
+    public interface OnBindViewHolderListener<Item, ViewHolder> {
+        /**
+         * Bind item to ViewHolder
+         *
+         * @param item     the item we want to bind
+         * @param holder   the ViewHolder
+         * @param position the position of the item
+         */
+        public void onBindViewHolder(@NonNull Item item, @NonNull ViewHolder holder, int position);
     }
 
-    public interface OnItemClickListener<T>{
+    /**
+     * define click action in ViewHolder.
+     * @param <T> the type of item we click
+     */
+    public interface OnItemClickListener<T> {
+        /**
+         * Handle event: click on item
+         *
+         * @param item The item that we click on
+         * @param v    The view where we click
+         */
         public void onClick(T item, View v);
     }
 }
