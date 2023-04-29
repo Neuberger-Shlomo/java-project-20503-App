c7c1d8f2494e08f4a1bdba4a4c2b3b39f767beea
Yossi Shemer <26yossi262@gmail.com>
2023-04-29 14:07:58 +0300

    added comments

--- app/src/main/java/com/example/myapplication/api/Requests/AuthedJsonObjectRequest.java
+++ app/src/main/java/com/example/myapplication/api/Requests/AuthedJsonObjectRequest.java
@@ -10,12 +10,21 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ add authentication headers to the request
+ */
 public class AuthedJsonObjectRequest extends JsonObjectRequest {
 
     private final String token;
     private final String userId;
-
+    /**
+     * constructor that adds authentication headers to the request.
+     * @param url the url to send the request
+     *  @param userId the user id
+     @param token the user token
+      * @param listener  listener handle successful responses
+     * @param errorListener  listener that handle errors
+     */
     public AuthedJsonObjectRequest(String url, String userId,
                                    String token, Response.Listener<JSONObject> listener,
                                    @Nullable Response.ErrorListener errorListener) {
@@ -23,7 +32,16 @@ public class AuthedJsonObjectRequest extends JsonObjectRequest {
         this.token = token;
         this.userId = userId;
     }
-
+    /**
+     * constructor that adds authentication headers to the request.
+     * @param method the http method for the request (for example GET, Post..)
+     * @param url the url to send the request
+     * @param userId the user id
+     * @param token the user token
+     * @param jsonRequest The jaon request body
+     * @param listener  listener = handle successful responses
+     * @param errorListener  listener = handle errors
+     */
     public AuthedJsonObjectRequest(int method, String url, String userId,
                                    String token, @Nullable JSONObject jsonRequest,
                                    Response.Listener<JSONObject> listener,
@@ -32,7 +50,11 @@ public class AuthedJsonObjectRequest extends JsonObjectRequest {
         this.token = token;
         this.userId = userId;
     }
-
+    /**
+     add authentication headers to request
+     * @return  map of headers that inclde in the request
+     * @throws AuthFailureError if error
+     */
     @Override
     public Map<String, String> getHeaders() throws AuthFailureError {
 
