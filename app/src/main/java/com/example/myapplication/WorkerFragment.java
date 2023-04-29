32a4295fcfbdba14d2fa53a5841bf245905dbb1b
Yossi Shemer <26yossi262@gmail.com>
2023-04-29 21:38:50 +0300

    added comments

--- app/src/main/java/com/example/myapplication/WorkerFragment.java
+++ app/src/main/java/com/example/myapplication/WorkerFragment.java
@@ -1,5 +1,9 @@
 package com.example.myapplication;
+/**
 
+ This class represents a fragment in the Android application that displays information about a worker.
+ It displays the worker's profile and allows a super admin to promote or demote the worker's role level.
+ */
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Application;
@@ -44,7 +48,13 @@ public class WorkerFragment extends Fragment implements ViewModelStoreOwner {
     Profile      profile;
     ProfileModel profileModel;
     RequestQueue queue;
-
+    /**
+     inflate the view  and set the listeners
+     * @param inflater
+     * @param container             the parent view
+     * @param savedInstanceState saved previous state. so we can restore it.
+     * @return the view for the fregment
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -61,7 +71,7 @@ public class WorkerFragment extends Fragment implements ViewModelStoreOwner {
                         setProfile(p);
                     }
                 });
-
+        //check changes in the user state
         new ViewModelProvider(getActivity()).get(UserViewModel.class)
                 .getUserState().observe(getViewLifecycleOwner(), u -> {
                     user = u;
@@ -71,14 +81,14 @@ public class WorkerFragment extends Fragment implements ViewModelStoreOwner {
                                                        () -> {
                                                        }, this::onUserArrived));
                 });
-
+        //inflate the view
         binding = FragmentWorkerViewBinding.inflate(inflater, container, false);
         if (arguments == null) {
             NavHostFragment.findNavController(WorkerFragment.this).popBackStack();
             return null;
         }
         employeeID = arguments.getString(WORKER_ID_KEY, "");
-
+        //if no id was passed, go back to the previous fragment
         if (employeeID == null) {
             NavHostFragment.findNavController(WorkerFragment.this).popBackStack();
             return null;
@@ -89,28 +99,39 @@ public class WorkerFragment extends Fragment implements ViewModelStoreOwner {
 
         return binding.getRoot();
     }
-
+    /**
+     * Set the profile for to update or downgrade the worker role level
+     *
+     * @param p the profile received from the server
+     */
     @SuppressLint("SetTextI18n")
     private void setProfile(Profile p) {
         binding.tvId.setText("Profile id: " + p.getId());
         binding.tvRole.setText("Profile role is: " + p.getMaxRole().toString());
         binding.tvProfile.setText(p.toPrettyString());
         profile = p;
+        //downgrade worker role level to manager
         if (user.isSuperAdmin())
             if (p.getMaxRole().ordinal() < RoleLevel.MANAGER.ordinal()) {
                 binding.btnSetAdmin.setText("Make manager");
                 binding.btnSetAdmin.setVisibility(View.VISIBLE);
                 binding.btnSetAdmin.setOnClickListener(this::onPromoteClicked);
+                //downgrade worker role level to normal
             } else if (p.getMaxRole().ordinal() ==
                        RoleLevel.MANAGER.ordinal() && p.getMaxRole() != RoleLevel.SUPER_ADMIN) {
                 binding.btnSetAdmin.setText("Make normal");
                 binding.btnSetAdmin.setVisibility(View.VISIBLE);
                 binding.btnSetAdmin.setOnClickListener(this::onDemoteClicked);
             } else {
+                //hide button
                 binding.btnSetAdmin.setVisibility(View.GONE);
             }
     }
 
+    /**
+     * Handle Demote button clicked.
+     * @param view
+     */
     private void onDemoteClicked(View view) {
         queue.add(
                 UsersApi.demoteUserByID(
@@ -126,7 +147,10 @@ public class WorkerFragment extends Fragment implements ViewModelStoreOwner {
                             }
                         }));
     }
-
+    /**
+     * Handle promote button clicked.
+     * @param view
+     */
     private void onPromoteClicked(View view) {
         queue.add(
                 UsersApi.promoteUserByID(
@@ -142,9 +166,15 @@ public class WorkerFragment extends Fragment implements ViewModelStoreOwner {
                             }
                         }));
     }
-
+    /**
+     *handle when the user data received from the server
+     * @param jsonObject jsonarray contain the user data.
+     * @param responseError if error
+     * @param throwable if throwable
+     */
     private void onUserArrived(JSONArray jsonObject, Api.ResponseError responseError,
                                Throwable throwable) {
+
         if (responseError != null || throwable != null) {
             new AlertDialog.
                     Builder(requireContext()).setTitle("Some error occurred")
@@ -171,7 +201,9 @@ public class WorkerFragment extends Fragment implements ViewModelStoreOwner {
             }
         }
     }
-
+    /**
+     * ViewModel for the Profile.
+     */
     public static class ProfileModel extends AndroidViewModel {
         private final MutableLiveData<Profile> profileState =
                 new MutableLiveData<>(new Profile());
@@ -180,15 +212,16 @@ public class WorkerFragment extends Fragment implements ViewModelStoreOwner {
             super(application);
         }
 
+        /**  @return the LiveData object for the Profile */
         public LiveData<Profile> getProfileState() {
             return profileState;
         }
-
+        //add user to the profile
         public void addUser(User u) {
             profileState.getValue().getUsers().add(u);
             profileState.setValue(profileState.getValue());
         }
-
+        //set profile for the view
         public void setProfile(Profile p) {
             if (!Objects.equals(p, profileState.getValue())) {
                 profileState.setValue(p);
